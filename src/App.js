a2e8dc96cfa9068ddf3b19cb196a97774be168ec
shentong <tong.shen@trip.com>
2021-10-21 23:37:05 +0800

    改进了一下

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import {ImageUploader, Space, Toast, Dialog, Button, Image} from 'antd-mobile'
+import {ImageUploader, Space, Toast, Dialog, Button, Image as ImageAntd} from 'antd-mobile'
 import {Divider} from 'antd-mobile'
 import {baseUrl} from "./Base";
 import {get} from "./utils/request";
@@ -15,7 +15,7 @@ function App() {
 
   async function requestFile(file) {
 
-    const imgInfo = await getImgInfo();
+    const imgInfo = await getImgInfo(file);
     let width;
     let height;
     const maxWidth = 640;
@@ -34,9 +34,9 @@ function App() {
           method: 'post',
           body: formData,
         }).then(response => response.json())
-        .then((data) => {
-          resolve(data);
-        }).catch(e => {
+          .then((data) => {
+            resolve(data);
+          }).catch(e => {
           reject(e);
         });
       })
@@ -44,23 +44,22 @@ function App() {
   }
 
   const getImgInfo = async (file) => {
-    console.log(file.width);
-
     return new Promise((resolve, reject) => {
-      var img = new Image();
-      var reader = new FileReader()
-      // 通过此方法可以让图片显示
-      reader.readAsDataURL(file)
-      reader.onload = function () {
-        img.src = this.result
-      }
-      // 加载完成执行
-      img.onload = function () {
-        resolve({width: img.width, height: img.height});
-      }
+
+      let filereader = new FileReader();
+      filereader.onload = e => {
+        let src = e.target.result;
+        const image = new Image();
+        image.onload = function () {
+          resolve({width: this.width, height: this.height});
+        };
+        image.onerror = reject;
+        image.src = src;
+      };
+      filereader.readAsDataURL(file);
     })
 
-  }
+  };
 
   const unloadImg = async (file) => {
     const resp = await requestFile(file);
@@ -73,46 +72,57 @@ function App() {
       return;
     }
     const url = `${baseUrl}/fun-img/trans?url=${encodeURIComponent(
-        fileList[0].url)}&type=${type}`
+      fileList[0].url)}&type=${type}`
     const resp = await get(url);
     setResultImg(resp && resp.data ? resp.data : undefined);
   }
 
   return (
-      <div className="App">
-
+    <div className="App">
+      <Space justify='center' block>
         <h1>人像转动漫</h1>
-        <Divider/>
-        <div>请上传图片</div>
-        <br/>
+      </Space>
+      <Divider/>
+      <Space justify='center' block>
+        <h5>请上传图片</h5>
+      </Space>
+      <Space justify='center' block>
         <ImageUploader
-            maxCount={1}
-            value={fileList}
-            onChange={setFileList}
-            upload={unloadImg}
-        />
+          maxCount={1}
+          value={fileList}
+          onChange={setFileList}
+          upload={unloadImg}
+        >
+        </ImageUploader>
+      </Space>
+      <br/>
+      <div>
+        <Space justify='center' block>
+          <Button color='primary'
+                  onClick={(e) => changeType("anime")}>动漫风</Button>
+          <Button color='primary'
+                  onClick={(e) => changeType("3d")}>迪士尼风</Button>
+          <Button color='primary'
+                  onClick={(e) => changeType("handdrawn")}>手绘风</Button>
+        </Space>
         <br/>
-        <div>
-          <Space>
-            <Button color='primary'
-                    onClick={(e) => changeType("anime")}>动漫风</Button>
-            <Button color='primary'
-                    onClick={(e) => changeType("3d")}>迪士尼风</Button>
-            <Button color='primary'
-                    onClick={(e) => changeType("handdrawn")}>手绘风</Button>
-            <Button color='primary'
-                    onClick={(e) => changeType("sketch")}>素描风</Button>
-            <Button color='primary'
-                    onClick={(e) => changeType("artstyle")}>艺术风</Button>
-          </Space>
-        </div>
-        <div>
-          {
-            resultImg && <Image src={resultImg}/>
-          }
-
-        </div>
+        <Space justify='center' block>
+          <Button color='primary'
+                  onClick={(e) => changeType("sketch")}>素描风</Button>
+          <Button color='primary'
+                  onClick={(e) => changeType("artstyle")}>艺术风</Button>
+        </Space>
       </div>
+      <br/>
+      <div>
+        <Space justify='center' block>
+          {
+
+            resultImg && <ImageAntd src={resultImg}/>
+          }
+        </Space>
+      </div>
+    </div>
   );
 }
 
